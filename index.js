3616e906ad56c3511aeb49294f0c66ab7fc82c24
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-22 16:12:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number) {
+  if number > 400
+    return "This one's on me!"
+    
+}
 
+
+function ternaryCheckCity() {
+  
+}
+
+function switchOnCharmFromTip() {
+  
+}
